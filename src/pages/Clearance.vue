e05b4ed063b925367ce46064109b727b10e4908d
Ned Palacios <npdoesmc@gmail.com>
2022-04-14 10:11:31 +0800

    improve clearance page

--- src/pages/Clearance.vue
+++ src/pages/Clearance.vue
@@ -54,14 +54,64 @@
 
         <div v-if="isResolved && data.items.length" class="bg-gray-50 dark:bg-uic-800 shadow border dark:border-uic-600 rounded-lg">
           <div class="flex flex-col divide-y dark:divide-uic-600">
-            <div 
-              :key="'item_' + i" v-for="(clearanceItem, i) in data.items" 
-              class="flex justify-between cursor-pointer px-6 py-4 transition-colors hover:bg-gray-100 dark:hover:bg-uic-700">
-              <p>{{ clearanceItem.label }}</p>
-              <icon-checkmark-circle-outline v-if="clearanceItem.status == 'cleared'" class="text-xl text-green-600" />
-              <icon-close-circle-outline v-else-if="clearanceItem.status == 'not_cleared'" class="text-xl text-red-600" />
-              <icon-unknown-circle-outline v-else class="text-xl text-gray-600" />
-            </div>
+            <self-modal 
+              :key="'item_' + i" v-for="(clearanceItem, i) in data.items"
+              :title="clearanceItem.label + ' Requirements'">
+              <template #default="{ openModal }">
+                <div
+                  @click="openModal"
+                  class="flex justify-between cursor-pointer px-6 py-4 transition-colors hover:bg-gray-100 dark:hover:bg-uic-700">
+                  <p>{{ clearanceItem.label }}</p>
+                  <div class="flex space-x-2 items-center">
+                    <p class="font-bold">
+                      <template v-if="clearanceItem.status === 'not_cleared'">
+                        {{ `${clearedRequirementsCount(clearanceItem)} / ${clearanceItem.requirements.length}` }}
+                      </template>
+                      <span class="<md:hidden ml-1">
+                        {{ clearanceItem.status === 'not_cleared' ? 'requirements' : statusText(clearanceItem.status) }}
+                      </span>
+                    </p>
+
+                    <component 
+                    :is="statusIcon(clearanceItem.status)" 
+                    :class="statusColor(clearanceItem.status)" class="text-xl" />
+                  </div>
+                </div>
+              </template>
+
+              <template #modal-content>
+                <div v-if="isLoading" class="flex justify-center py-8">
+                  <loader class="h-14 w-14" />
+                </div>
+
+                <div v-else class="flex flex-col divide-y">
+                  <div class="pl-0 py-4 text-center">
+                    <skeleton custom-class="w-16 h-4 bg-gray-200 mb-2">
+                      <p class="text-lg mb-1">Status</p>
+                    </skeleton>
+                    <skeleton custom-class="w-8 h-3.5 bg-gray-200">
+                      <p class="font-semibold text-4xl" :class="statusColor(clearanceItem.status)">
+                        {{ statusText(clearanceItem.status) }}
+                      </p>
+                    </skeleton>
+                  </div>
+
+                  <div v-if="clearanceItem.requirements.length == 0" class="text-center text-gray-500 py-3">
+                    <span class="block text-2xl">No requirements found.</span>
+                  </div>
+
+                  <div v-else class="flex flex-col divide-y">
+                    <div v-for="r in clearanceItem.requirements" class="flex justify-between py-2">
+                      <p>{{ r.remarks }}</p>
+                      <div class="flex space-x-2 items-center">
+                        <p class="font-bold">{{ statusText(r.status) }}</p>
+                        <component :is="statusIcon(r.status)" :class="statusColor(r.status)" />
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </template>
+            </self-modal>
           </div>
         </div>
       </div>
@@ -69,13 +119,16 @@
   </dashboard-header>
 </template>
 
-<script>
+<script lang="ts">
+import { ClearanceItem } from '@myuic-api/types';
 import IconCheckmarkCircleOutline from '~icons/ion/ios-checkmark-circle-outline';
 import IconCloseCircleOutline from '~icons/ion/ios-close-circle-outline';
 import IconUnknownCircleOutline from '~icons/ion/remove-circle-outline';
 import DashboardHeader from '../components/ui/DashboardHeader.vue';
+import Loader from '../components/ui/Loader.vue';
 import LoadingContainer from '../components/ui/LoadingContainer.vue';
 import PromiseLoader from '../components/ui/PromiseLoader.vue';
+import SelfModal from '../components/ui/SelfModal.vue';
 import Skeleton from '../components/ui/Skeleton.vue';
 import { useClearanceStore, useStudentStore } from '../stores/studentStore';
 import { catchAndNotifyError } from '../utils';
@@ -88,15 +141,49 @@ export default {
     PromiseLoader,
     LoadingContainer,
     DashboardHeader,
-    Skeleton
+    Skeleton,
+    Loader,
+    SelfModal,
   },
   setup() {
     const clearanceStore = useClearanceStore();
     const studentStore = useStudentStore();
+    const statusText = (status: 'cleared' | 'not_cleared' | 'unknown') => {
+      return status == 'cleared' 
+        ? 'Cleared'
+        : status == 'not_cleared'
+        ? 'Not cleared'
+        : 'Unknown';
+    }
+
+    const statusIcon = (status: 'cleared' | 'not_cleared' | 'unknown') => {
+      return status == 'cleared' 
+        ? 'icon-checkmark-circle-outline'
+        : status == 'not_cleared'
+        ? 'icon-close-circle-outline'
+        : 'icon-unknown-circle-outline';
+    }
+
+    const statusColor = (status: 'cleared' | 'not_cleared' | 'unknown') => {
+      return status == 'cleared' 
+        ? 'text-green-600'
+        : status == 'not_cleared'
+        ? 'text-red-400'
+        : 'text-gray-600';
+    }
+
+    const clearedRequirementsCount = (item: ClearanceItem) => {
+      return item.requirements
+        .filter(r => r.status === 'cleared' || r.status === 'promisory').length;
+    }
 
     return {
       clearanceStore,
-      studentStore
+      studentStore,
+      clearedRequirementsCount,
+      statusIcon,
+      statusColor,
+      statusText
     }
   },
   methods: {
