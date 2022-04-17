ca3f70ddb90c50e4f065448a26cd8d20e0a68d51
Ned Palacios <npdoesmc@gmail.com>
2022-04-17 17:36:16 +0800

    report: transition from pinia to vue-query

--- src/pages/Report.vue
+++ src/pages/Report.vue
@@ -25,115 +25,112 @@
       </self-modal>
     </div>
 
-    <promise-loader 
-      :promise="studentStore.getAcademicRecords()" 
-      v-slot="{ isResolved, isPending }">
-      <loading-container :is-loading="isPending" v-slot="{ isLoading }">
-        <!-- TODO: package into skeleton -->
-        <section 
-          v-if="!isLoading"
-          :class="{'border-primary-400': j === 0}" 
-          class="bg-white dark:bg-primary-800 border dark:border-primary-600 rounded-xl shadow-md">
-          <div 
-            :class="[j === 0 ? 'bg-gradient-to-r from-primary-100 to-primary-50 dark:from-primary-700 dark:to-primary-900' : 'border-b dark:border-primary-600']" 
-            class="rounded-t-xl px-6 py-4 flex flex-col md:flex-row justify-between items-center pb-4">
-            <div class="flex flex-row text-center md:flex-col md:text-left <md:space-x-1 <md:mb-2">
-              <h2 class="font-semibold md:text-2xl">
-                {{ r.semester }}
-              </h2>
-              <p>{{ r.year }}</p>
-            </div>
-            <div class="flex flex-row space-x-6 md:text-right">
-              <div class="flex flex-col text-center md:flex-row md:space-x-2 md:text-right">
-                <span class="text-lg block cursor-default">Units</span>
-                <p class="font-semibold text-3xl md:text-5xl">{{ overallUnits[j] }}</p>
-              </div>
-              <div class="flex flex-col text-center md:flex-row md:space-x-2 md:text-right">
-                <span 
-                  class="text-lg block underline underline-dotted underline-offset-2 cursor-default" 
-                  v-tooltip="'General Weighted Average'">GWA</span>
-                <p class="font-semibold text-3xl md:text-5xl">{{ overallAverages[j] }}</p>
-              </div>
-            </div>
+    <loading-container :is-loading="isLoading" v-slot="{ isLoading }">
+      <!-- TODO: package into skeleton -->
+      <section 
+        v-if="!isLoading"
+        :key="'sem_' + j" v-for="(r, j) in latestAcademicRecords" 
+        :class="{'border-primary-400': j === 0}" 
+        class="bg-white dark:bg-primary-800 border dark:border-primary-600 rounded-xl shadow-md">
+        <div 
+          :class="[j === 0 ? 'bg-gradient-to-r from-primary-100 to-primary-50 dark:from-primary-700 dark:to-primary-900' : 'border-b dark:border-primary-600']" 
+          class="rounded-t-xl px-6 py-4 flex flex-col md:flex-row justify-between items-center pb-4">
+          <div class="flex flex-row text-center md:flex-col md:text-left <md:space-x-1 <md:mb-2">
+            <h2 class="font-semibold md:text-2xl">
+              {{ semesterDisplayNames[j].semester }}
+            </h2>
+            <p>{{ semesterDisplayNames[j].year }}</p>
           </div>
-          <div class="rounded-b-xl">
-            <div class="flex flex-col divide-y dark:divide-primary-600">
-              <div :key="'sem_' + j + '_sub_' + i" v-for="(ar, i) in r.reports" class="flex items-center">
-                <div class="<md:hidden w-1/9 px-3 md:px-6 py-2 md:py-4">
-                  <p class="font-semibold">{{ ar.code }}</p>
-                </div>
-                <div class="w-5/9 md:w-4/9 px-3 md:px-6 py-2 md:py-4">
-                  <p class="font-semibold text-sm md:hidden">{{ ar.code }}</p>
-                  <p class="sm:text-lg font-semibold">{{ ar.name }}</p>
-                  <p class="text text-gray-600 dark:text-primary-100">
-                    {{ ar.units }} units / {{ ar.type == 'Lec' ? 'Lecture' : ar.type == 'Lab' ? 'Laboratory' : 'Unknown' }}
-                  </p>
-                </div>
-                <div class="w-1/9 px-2 md:px-4 py-2 md:py-3 <md:text-center">
-                  <p class="text-xl md:text-3xl font-semibold">{{ ar.prelimGrade ?? '--' }}</p>
-                  <p class="text-gray-600 dark:text-primary-100 text-sm underline underline-dotted underline-offset-2 md:hidden" v-tooltip="'Prelim'">P</p>
-                  <p class="text-gray-600 dark:text-primary-100 hidden md:block">Prelim</p>
-                </div>
-                <div class="w-1/9 px-2 md:px-4 py-2 md:py-3 <md:text-center">
-                  <p class="text-xl md:text-3xl font-semibold">{{ ar.midtermGrade ?? '--' }}</p>
-                  <p class="text-gray-600 dark:text-primary-100 text-sm underline underline-dotted underline-offset-2 md:hidden" v-tooltip="'Midterms'">M</p>
-                  <p class="text-gray-600 dark:text-primary-100 hidden md:block">Midterms</p>
-                </div>
-                <div class="w-1/9 px-2 md:px-4 py-2 border-r md:py-3 <md:text-center">
-                  <p class="text-xl md:text-3xl font-semibold">{{ ar.finalsGrade ?? '--' }}</p>
-                  <p class="text-gray-600 dark:text-primary-100 text-sm underline underline-dotted underline-offset-2 md:hidden" v-tooltip="'Finals'">F</p>
-                  <p class="text-gray-600 dark:text-primary-100 hidden md:block">Finals</p>
-                </div>
-                <div class="w-1/9 px-2 md:px-4 py-2 md:py-3 <md:text-center flex flex-col">
-                  <p class="text-xl text-primary-800 dark:text-primary-200 md:text-3xl font-semibold <md:my-auto">{{ ar.overallGrade ?? '--' }}</p>
-                  <p class="text-gray-600 dark:text-primary-100 <md:hidden">Overall</p>
-                </div>
-              </div>
-            </div>
-          </div>
-        </section>
-        <section 
-          v-else 
-          :key="'sem_' + j" 
-          v-for="j in 2" 
-          :class="{'border-primary-400': j === 1}" 
-          class="bg-white dark:bg-primary-800 border dark:border-primary-600 rounded-xl shadow-md -z-1">
-          <div 
-            :class="[j === 1 ? 'bg-gradient-to-r from-primary-100 to-primary-50 dark:from-primary-700 dark:to-primary-900' : 'border-b  dark:border-primary-600']" 
-            class="rounded-t-xl px-6 py-4 flex flex-col md:flex-row justify-between items-center pb-4">
-            <div class="flex flex-row text-center md:flex-col md:text-left <md:space-x-1 <md:mb-2 animate-pulse">
-              <div class="w-24 h-4 md:h-6 rounded-lg mb-2" :class="[j === 1 ? 'bg-primary-300' : 'bg-gray-300']"></div>
-              <div class="w-16 h-4 rounded-lg" :class="[j === 1 ? 'bg-primary-300' : 'bg-gray-300']"></div>
+          <div class="flex flex-row space-x-6 md:text-right">
+            <div class="flex flex-col text-center md:flex-row md:space-x-2 md:text-right">
+              <span class="text-lg block cursor-default">Units</span>
+              <p class="font-semibold text-3xl md:text-5xl">{{ overallUnits[j] }}</p>
             </div>
             <div class="flex flex-col text-center md:flex-row md:space-x-2 md:text-right">
-              <span class="text-lg block">Average</span>
-              <div class="w-18 h-7.5 md:h-12 rounded-lg animate-pulse" :class="[j === 1 ? 'bg-primary-300' : 'bg-gray-300']"></div>
+              <span 
+                class="text-lg block underline underline-dotted underline-offset-2 cursor-default" 
+                v-tooltip="'General Weighted Average'">GWA</span>
+              <p class="font-semibold text-3xl md:text-5xl">{{ overallAverages[j] }}</p>
             </div>
           </div>
-          <div class="rounded-b-xl">
-            <div class="flex flex-col divide-y dark:divide-primary-600">
-              <div :key="'sem_' + j + '_sub_' + i" v-for="i in 4" class="flex items-center animate-pulse">
-                <div class="<md:hidden w-1/9 px-3 md:px-6 py-2 md:py-4">
-                  <div class="w-6 md:w-11 h-5 md:h-7.5 mt-1 mb-3 rounded-lg bg-gray-400"></div>
-                </div>
-                <div class="w-5/9 px-3 md:px-6 py-2 md:py-4">
-                  <div class="w-18 sm:h-4.5 h-4 md:h-5 mt-1 mb-2 rounded-lg bg-gray-200"></div>
-                  <div class="w-10 h-4 rounded-lg bg-gray-200"></div>
-                </div>
-                <div :key="'g_' + jj" v-for="jj in 3" class="w-1/9 px-2 md:px-4 py-2 md:py-3 <md:text-center animate-pulse">
-                  <div class="w-6 md:w-11 h-5 md:h-7.5 mt-1 mb-3 rounded-lg bg-gray-400"></div>
-                  <div class="w-5 md:w-14 <md:h-3.5 h-4 rounded-lg bg-gray-200"></div>
-                </div>
-                <div class="flex flex-col w-1/9 px-2 md:px-4 py-2 md:py-3 <md:text-center animate-pulse">
-                  <div class="w-6 md:w-11 h-5 md:h-7.5 mt-1 mb-3 rounded-lg bg-primary-400"></div>
-                  <div class="w-5 md:w-14 <md:h-3.5 h-4 rounded-lg bg-gray-200"></div>
-                </div>
+        </div>
+        <div class="rounded-b-xl">
+          <div class="flex flex-col divide-y dark:divide-primary-600">
+            <div :key="'sem_' + j + '_sub_' + i" v-for="(ar, i) in r.reports" class="flex items-center">
+              <div class="<md:hidden w-1/9 px-3 md:px-6 py-2 md:py-4">
+                <p class="font-semibold">{{ ar.code }}</p>
+              </div>
+              <div class="w-5/9 md:w-4/9 px-3 md:px-6 py-2 md:py-4">
+                <p class="font-semibold text-sm md:hidden">{{ ar.code }}</p>
+                <p class="sm:text-lg font-semibold">{{ ar.name }}</p>
+                <p class="text text-gray-600 dark:text-primary-100">
+                  {{ ar.units }} units / {{ ar.type == 'Lec' ? 'Lecture' : ar.type == 'Lab' ? 'Laboratory' : 'Unknown' }}
+                </p>
+              </div>
+              <div class="w-1/9 px-2 md:px-4 py-2 md:py-3 <md:text-center">
+                <p class="text-xl md:text-3xl font-semibold">{{ ar.prelimGrade ?? '--' }}</p>
+                <p class="text-gray-600 dark:text-primary-100 text-sm underline underline-dotted underline-offset-2 md:hidden" v-tooltip="'Prelim'">P</p>
+                <p class="text-gray-600 dark:text-primary-100 hidden md:block">Prelim</p>
+              </div>
+              <div class="w-1/9 px-2 md:px-4 py-2 md:py-3 <md:text-center">
+                <p class="text-xl md:text-3xl font-semibold">{{ ar.midtermGrade ?? '--' }}</p>
+                <p class="text-gray-600 dark:text-primary-100 text-sm underline underline-dotted underline-offset-2 md:hidden" v-tooltip="'Midterms'">M</p>
+                <p class="text-gray-600 dark:text-primary-100 hidden md:block">Midterms</p>
+              </div>
+              <div class="w-1/9 px-2 md:px-4 py-2 border-r md:py-3 <md:text-center">
+                <p class="text-xl md:text-3xl font-semibold">{{ ar.finalsGrade ?? '--' }}</p>
+                <p class="text-gray-600 dark:text-primary-100 text-sm underline underline-dotted underline-offset-2 md:hidden" v-tooltip="'Finals'">F</p>
+                <p class="text-gray-600 dark:text-primary-100 hidden md:block">Finals</p>
+              </div>
+              <div class="w-1/9 px-2 md:px-4 py-2 md:py-3 <md:text-center flex flex-col">
+                <p class="text-xl text-primary-800 dark:text-primary-200 md:text-3xl font-semibold <md:my-auto">{{ ar.overallGrade ?? '--' }}</p>
+                <p class="text-gray-600 dark:text-primary-100 <md:hidden">Overall</p>
               </div>
             </div>
           </div>
-        </section>
-      </loading-container>
-    </promise-loader>
+        </div>
+      </section>
+      <section 
+        v-else 
+        :key="'sem_' + j" 
+        v-for="j in 2" 
+        :class="{'border-primary-400': j === 1}" 
+        class="bg-white dark:bg-primary-800 border dark:border-primary-600 rounded-xl shadow-md -z-1">
+        <div 
+          :class="[j === 1 ? 'bg-gradient-to-r from-primary-100 to-primary-50 dark:from-primary-700 dark:to-primary-900' : 'border-b  dark:border-primary-600']" 
+          class="rounded-t-xl px-6 py-4 flex flex-col md:flex-row justify-between items-center pb-4">
+          <div class="flex flex-row text-center md:flex-col md:text-left <md:space-x-1 <md:mb-2 animate-pulse">
+            <div class="w-24 h-4 md:h-6 rounded-lg mb-2" :class="[j === 1 ? 'bg-primary-300' : 'bg-gray-300']"></div>
+            <div class="w-16 h-4 rounded-lg" :class="[j === 1 ? 'bg-primary-300' : 'bg-gray-300']"></div>
+          </div>
+          <div class="flex flex-col text-center md:flex-row md:space-x-2 md:text-right">
+            <span class="text-lg block">Average</span>
+            <div class="w-18 h-7.5 md:h-12 rounded-lg animate-pulse" :class="[j === 1 ? 'bg-primary-300' : 'bg-gray-300']"></div>
+          </div>
+        </div>
+        <div class="rounded-b-xl">
+          <div class="flex flex-col divide-y dark:divide-primary-600">
+            <div :key="'sem_' + j + '_sub_' + i" v-for="i in 4" class="flex items-center animate-pulse">
+              <div class="<md:hidden w-1/9 px-3 md:px-6 py-2 md:py-4">
+                <div class="w-6 md:w-11 h-5 md:h-7.5 mt-1 mb-3 rounded-lg bg-gray-400"></div>
+              </div>
+              <div class="w-5/9 px-3 md:px-6 py-2 md:py-4">
+                <div class="w-18 sm:h-4.5 h-4 md:h-5 mt-1 mb-2 rounded-lg bg-gray-200"></div>
+                <div class="w-10 h-4 rounded-lg bg-gray-200"></div>
+              </div>
+              <div :key="'g_' + jj" v-for="jj in 3" class="w-1/9 px-2 md:px-4 py-2 md:py-3 <md:text-center animate-pulse">
+                <div class="w-6 md:w-11 h-5 md:h-7.5 mt-1 mb-3 rounded-lg bg-gray-400"></div>
+                <div class="w-5 md:w-14 <md:h-3.5 h-4 rounded-lg bg-gray-200"></div>
+              </div>
+              <div class="flex flex-col w-1/9 px-2 md:px-4 py-2 md:py-3 <md:text-center animate-pulse">
+                <div class="w-6 md:w-11 h-5 md:h-7.5 mt-1 mb-3 rounded-lg bg-primary-400"></div>
+                <div class="w-5 md:w-14 <md:h-3.5 h-4 rounded-lg bg-gray-200"></div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </loading-container>
   </dashboard-header>
 </template>
 
@@ -141,7 +138,7 @@
 import DashboardHeader from '../components/ui/DashboardHeader.vue';
 import LoadingContainer from '../components/ui/LoadingContainer.vue';
 import PromiseLoader from '../components/ui/PromiseLoader.vue';
-import { useStudentStore } from '../stores/studentStore';
+import { generateAcademicRecordsPDF, useAcademicRecordQuery, useAcademicRecordQueryUtilities } from '../stores/academicRecordStore';
 import IconPrint from '~icons/ion/print';
 import { catchAndNotifyError } from '../utils';
 import SelfModal from '../components/ui/SelfModal.vue';
@@ -152,9 +149,15 @@ import computationFormulaImg from '../assets/computation-formula.png';
 export default {
   components: { PromiseLoader, LoadingContainer, DashboardHeader, IconPrint, SelfModal },
   setup() {
-    const studentStore = useStudentStore();
-    return { 
-      studentStore,
+    const academicRecordQuery = useAcademicRecordQuery();
+    const { isLoading, latestAcademicRecords, overallAverages, semesterDisplayNames, overallUnits } = useAcademicRecordQueryUtilities(academicRecordQuery);
+
+    return {
+      latestAcademicRecords,
+      isLoading,
+      overallAverages,
+      overallUnits,
+      semesterDisplayNames,
       computationFormulaImg: ref(computationFormulaImg)
     }
   },
@@ -162,7 +165,7 @@ export default {
     async printPdf() {
       try {
         this.$notify({ type: 'info', text: 'Downloading PDF...' }, 10 * 1000);
-        const fileUrl = await this.studentStore.generatePDF();
+        const fileUrl = await generateAcademicRecordsPDF();
         const pdfPreviewTab = window.open(fileUrl, '_blank');
         if (pdfPreviewTab) {
           pdfPreviewTab.focus();
--- src/stores/studentStore.ts
+++ src/stores/studentStore.ts
@@ -3,8 +3,7 @@ import { client } from '../client';
 import { nameCase } from '@foundernest/namecase';
 import { destroy } from '../auth';
 import { RoutePath } from '@myuic-api/types';
-
-const semesterRegex = /((?:First|Second) Semester|Summer)\s(\d{4}-\d{4})/;
+import { semesterRegex } from '../utils';
 
 export const useStudentStore = defineStore('student', {
   state: () => ({
@@ -24,47 +23,13 @@ export const useStudentStore = defineStore('student', {
     isEmpty(state): boolean {
       return state.student == null || Object.keys(state.student).length == 0;
     },
+
     normalizedFirstName(state): string {
       if (this.isEmpty) return '';
       const splitted = state.student.firstName.split(' ');
       return nameCase(splitted[0]);
     },
-    isAcademicRecordEmpty(state): boolean {
-      return state.academicRecords == null || state.academicRecords.length == 0;
-    },
-    latestAcademicRecords(state): any[] {
-      if (this.isAcademicRecordEmpty) return [];
-      return state.academicRecords.reverse();
-    },
-    recordOverallUnits(): any[] {
-      if (this.isAcademicRecordEmpty) return [];
-      return this.latestAcademicRecords.map((r: any) => {
-        let totalUnits = 0;
-        for (const s of r.reports) {
-          const units = (s.units ?? 0);
-          totalUnits += units;
-        }
-        return totalUnits
-      });
-    },
-    recordOverallAverages(): any[] {
-      if (this.isAcademicRecordEmpty) return [];
-      return this.latestAcademicRecords.map((r: any) => {
-        // As per recommendation:
-        // (Sum of all product of overall/final grade and units) / total units
-        let totalUnits = 0;
-        let sumOfAllProduct = 0;
 
-        for (const s of r.reports) {
-          const units = (s.units ?? 0);
-          totalUnits += units;
-          sumOfAllProduct += ((s.overallGrade ?? 0) * units);
-        }
-
-        const avg = sumOfAllProduct / totalUnits;
-        return avg > 59 ? Math.round((avg + Number.EPSILON) * 100) / 100 : '--';
-      });
-    },
     currentSemester(state): any {
       return this.semesterList.find(s => s.id == state.currentSemesterId);
     },
@@ -77,23 +42,6 @@ export const useStudentStore = defineStore('student', {
         this.student = data;
       }
       return this.student;
-    },
-
-    async getAcademicRecords() {
-      if (this.isAcademicRecordEmpty) {
-        const data = await client.academicRecords();
-        if (data && data.reports) {
-          this.academicRecords = data.reports.map((r: any) => {
-            const parsedSemResults = semesterRegex.exec(r.label);
-            return {
-              ...r,
-              semester: parsedSemResults?.[1] ?? r.label,
-              year: parsedSemResults?.[2] ?? r.label
-            };
-          });
-        }
-      }
-      return this.academicRecords;
     },
 
     async getCurrentSemesterId() {
@@ -126,16 +74,6 @@ export const useStudentStore = defineStore('student', {
         }
       }
       return this.courseEvaluationList;
-    },
-
-    async generatePDF(): Promise<string> {
-      const data = await client.academicRecordsPDF();
-      if (data instanceof Blob && window.URL.createObjectURL) {
-        const fileUrl = window.URL.createObjectURL(data);
-        return fileUrl;
-      } else {
-        throw new Error('There was an error downloading the file.');
-      }
     },
 
     getSemesterInfoByID(semId: number): any {
