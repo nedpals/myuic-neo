682744c4bc9d2a3f6d3aa29534b748937395a88f
Ned Palacios <npdoesmc@gmail.com>
2022-04-23 16:58:10 +0800

    self-host web fonts used

--- src/App.vue
+++ src/App.vue
@@ -103,6 +103,43 @@ export default {
 </script>
 
 <style lang="postcss">
+/* commissioner-regular - latin */
+@font-face {
+  font-family: 'Commissioner';
+  font-style: normal;
+  font-weight: 400;
+  src: local(''),
+       url('/fonts/commissioner-v11-latin-regular.woff2') format('woff2'), /* Chrome 26+, Opera 23+, Firefox 39+ */
+       url('/fonts/commissioner-v11-latin-regular.woff') format('woff'); /* Chrome 6+, Firefox 3.6+, IE 9+, Safari 5.1+ */
+}
+/* commissioner-600 - latin */
+@font-face {
+  font-family: 'Commissioner';
+  font-style: normal;
+  font-weight: 600;
+  src: local(''),
+       url('/fonts/commissioner-v11-latin-600.woff2') format('woff2'), /* Chrome 26+, Opera 23+, Firefox 39+ */
+       url('/fonts/commissioner-v11-latin-600.woff') format('woff'); /* Chrome 6+, Firefox 3.6+, IE 9+, Safari 5.1+ */
+}
+/* commissioner-700 - latin */
+@font-face {
+  font-family: 'Commissioner';
+  font-style: normal;
+  font-weight: 700;
+  src: local(''),
+       url('/fonts/commissioner-v11-latin-700.woff2') format('woff2'), /* Chrome 26+, Opera 23+, Firefox 39+ */
+       url('/fonts/commissioner-v11-latin-700.woff') format('woff'); /* Chrome 6+, Firefox 3.6+, IE 9+, Safari 5.1+ */
+}
+/* commissioner-900 - latin */
+@font-face {
+  font-family: 'Commissioner';
+  font-style: normal;
+  font-weight: 900;
+  src: local(''),
+       url('/fonts/commissioner-v11-latin-900.woff2') format('woff2'), /* Chrome 26+, Opera 23+, Firefox 39+ */
+       url('/fonts/commissioner-v11-latin-900.woff') format('woff'); /* Chrome 6+, Firefox 3.6+, IE 9+, Safari 5.1+ */
+}
+
 body {
   @apply text-gray-700 font-sans-2 dark:text-white;
 }
