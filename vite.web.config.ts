cedba7ea1ce8000721425ae351b96f5fe98a22c3
Ned Palacios <npdoesmc@gmail.com>
2022-08-06 11:28:23 +0800

    web: make pwa automatically reload

--- vite.web.config.ts
+++ vite.web.config.ts
@@ -19,6 +19,7 @@ export default defineConfig({
   plugins: [
     ...plugins,
     VitePWA({
+      registerType: 'autoUpdate',
       includeManifestIcons: true,
       manifest: {
         background_color: "#FFFFFF",
