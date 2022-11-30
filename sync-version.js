b817e7dfe976d790469ddcb0fb24e6cc7bdeac7c
Ned Palacios <npdoesmc@gmail.com>
2022-11-30 14:41:56 +0800

    fix: app version not showing, robust .env manipulation, add cross-env

--- sync-version.js
+++ sync-version.js
@@ -1,7 +1,9 @@
 const plist = require('plist');
-const { readFileSync, writeFileSync } = require('fs');
+const { readFileSync, writeFileSync, existsSync } = require('fs');
 const { join } = require('path');
-const {execSync} = require("child_process");
+const { execSync } = require("child_process");
+const { parse: parseEnv } = require('dotenv');
+const stringifyEnv = require('dotenv-stringify');
 
 const package_json = require('./package.json');
 
@@ -17,12 +19,49 @@ const info = {
     })()
 }
 
+// Get .env
+const filesToLookFor = ['.env']
+if (process.env.NODE_ENV) {
+    filesToLookFor.unshift(`.env.${process.env.NODE_ENV}`);
+}
+
+/** @type {Record<string, any>} */
+let existingEnvData = {};
+let envToUse = '.env';
+
+for (const name of filesToLookFor) {
+    const path = join(__dirname, name);
+    if (existsSync(path)) {
+        existingEnvData = parseEnv(readFileSync(path));
+        envToUse = name;
+        break;
+    }
+}
+
+// converted to array to preserve entry orders
+const envEntries = Object.entries(existingEnvData).map(en => ({ name: en[0], value: en[1] ?? null }));
+
 // Set dotenv
-writeFileSync(join(__dirname, '.env'), `
-VITE_APP_VERSION_NAME="${info.APP_VERSION_NAME}"
-VITE_APP_VERSION_CODE="${info.APP_VERSION_CODE}"
-VITE_APP_BUILD_DATE="${info.APP_BUILD_DATE}"
-`.trim(), {
+function replaceEnvEntry(name, value) {
+    const idx = envEntries.findIndex(n => n.name == name);
+    if (idx == -1) {
+        envEntries.push({ name, value });
+        return;
+    }
+
+    envEntries[idx] = { ...envEntries[idx], value };
+}
+
+replaceEnvEntry('VITE_APP_VERSION_NAME', info.APP_VERSION_NAME);
+replaceEnvEntry('VITE_APP_VERSION_CODE', info.APP_VERSION_CODE);
+replaceEnvEntry('VITE_APP_BUILD_DATE', info.APP_BUILD_DATE);
+
+const finalEnvEntries = envEntries.reduce((p,v) => {
+    p[v.name] = v.value;
+    return p;
+}, {});
+
+writeFileSync(join(__dirname, envToUse), stringifyEnv(finalEnvEntries), {
     encoding: 'utf-8',
     flag: 'w'
 });
